e14634b0139124bc5316337bdc634a11ff86b450
Fernando Angeli <fernando_angeli@hotmail.com>
2022-05-10 16:21:02 -0300

    Mostrando dados e imagem do cliente para página de profile

---
+++ src/pages/profile/profile.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
+import { ClienteDto } from '../../models/cliente.dto';
+import { ClienteService } from '../../services/domain/cliente.service';
+import { StorageService } from '../../services/storage.service';
+
+@IonicPage()
+@Component({
+  selector: 'page-profile',
+  templateUrl: 'profile.html',
+})
+export class ProfilePage {
+
+  cliente: ClienteDto;
+
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams,
+              public storage: StorageService,
+              public clienteService : ClienteService ) {
+  }
+
+  ionViewDidLoad() {
+    let localUser = this.storage.getLocalUser();
+    if(localUser && localUser.email){
+      this.clienteService.findByEmail(localUser.email)
+      .subscribe(response => {
+        this.cliente = response;
+        this.getImageIfExists();
+      },
+      error => {});
+    }
+  }
+
+  getImageIfExists(){
+    this.clienteService.getImageFromBucket(this.cliente.id)
+    .subscribe(response => {
+      this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
+    },
+    error => {})
+  }
+
+}
