d55e1f91c4296cdd8afcdf070904aa9a2d10b16c
Fernando Angeli <fernando_angeli@hotmail.com>
2022-05-14 17:36:26 -0300

    Tela de escolha de pagamento

--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -53,7 +53,7 @@ export class PickAddressPage {
 
   nextPage(endereco: EnderecoDTO){
     this.pedido.enderecoDeEntrega = {id: endereco.id};
-    console.log(this.pedido)
+    this.navCtrl.push('PaymentPage', {pedido: this.pedido})
   }
 
 }
