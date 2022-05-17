fe82592cbb6f3be6823ca5f61d366f250ab04bb7
Fernando Angeli <fernando_angeli@hotmail.com>
2022-05-17 19:09:10 -0300

    Fazendo upload da foto de perfil

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,72 +1,91 @@
-import { Component } from '@angular/core';
-import { Camera, CameraOptions } from '@ionic-native/camera';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { API_CONFIG } from '../../config/api.config';
-import { ClienteDTO } from '../../models/cliente.dto';
-import { ClienteService } from '../../services/domain/cliente.service';
-import { StorageService } from '../../services/storage.service';
+import { Component } from "@angular/core";
+import { Camera, CameraOptions } from "@ionic-native/camera";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { API_CONFIG } from "../../config/api.config";
+import { ClienteDTO } from "../../models/cliente.dto";
+import { ClienteService } from "../../services/domain/cliente.service";
+import { StorageService } from "../../services/storage.service";
 
 @IonicPage()
 @Component({
-  selector: 'page-profile',
-  templateUrl: 'profile.html',
+  selector: "page-profile",
+  templateUrl: "profile.html",
 })
 export class ProfilePage {
-
   cliente: ClienteDTO;
-  picture : string;
+  picture: string;
   cameraOn: boolean = false;
 
-  constructor(public navCtrl: NavController, 
+  constructor(
+    public navCtrl: NavController,
     public navParams: NavParams,
     public storage: StorageService,
-    public clienteService : ClienteService,
-    public camera: Camera ) {
-}
+    public clienteService: ClienteService,
+    public camera: Camera
+  ) {}
 
   ionViewDidLoad() {
+    this.loadData();
+  }
+
+  loadData(){
     let localUser = this.storage.getLocalUser();
-    if(localUser && localUser.email){
-      this.clienteService.findByEmail(localUser.email)
-      .subscribe(response => {
-        this.cliente = response as ClienteDTO;
-        this.getImageIfExists();
-      },
-      error => {
-        if(error.status == 403){
-          this.navCtrl.setRoot('HomePage');
+    if (localUser && localUser.email) {
+      this.clienteService.findByEmail(localUser.email).subscribe(
+        (response) => {
+          this.cliente = response as ClienteDTO;
+          this.getImageIfExists();
+        },
+        (error) => {
+          if (error.status == 403) {
+            this.navCtrl.setRoot("HomePage");
+          }
         }
-      });
-    }
-    else{
-      this.navCtrl.setRoot('HomePage')
+      );
+    } else {
+      this.navCtrl.setRoot("HomePage");
     }
   }
 
-  getImageIfExists(){
-    this.clienteService.getImageFromBucket(this.cliente.id)
-    .subscribe(response => {
-      this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
-    },
-    error => {})
+  getImageIfExists() {
+    this.clienteService.getImageFromBucket(this.cliente.id).subscribe(
+      (response) => {
+        this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
+      },
+      (error) => {}
+    );
   }
 
-  getCameraPicture(){
-    
+  getCameraPicture() {
     this.cameraOn = true;
 
     const options: CameraOptions = {
       quality: 100,
       destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.PNG,
-      mediaType: this.camera.MediaType.PICTURE
-    }
-    
-    this.camera.getPicture(options).then((imageData) => {
-    this.picture = 'data:image/png;base64,' + imageData;
-    this.cameraOn = false;  
-    }, (err) => {
-    });
+      mediaType: this.camera.MediaType.PICTURE,
+    };
+
+    this.camera.getPicture(options).then(
+      (imageData) => {
+        this.picture = "data:image/png;base64," + imageData;
+        this.cameraOn = false;
+      },
+      (err) => {}
+    );
   }
 
+  sendPicture() {
+    this.clienteService.uploadPicture(this.picture)
+    .subscribe(response => {
+      this.picture = null;
+      this.loadData();
+    },
+    error => {
+    })
+  }
+
+  cancel(){
+    this.picture = null;
+  }
 }
